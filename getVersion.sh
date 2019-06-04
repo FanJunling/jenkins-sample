b87367ed7af0a9c29db2f9abba4ab5db8a5f0c98
FanJunling <32149090+FanJunling@users.noreply.github.com>
2019-06-04 17:29:32 +0800

    Update getVersion.sh

--- getVersion.sh
+++ getVersion.sh
@@ -1,2 +1,2 @@
 
-kubectl rollout history deploy/jenkins-demo -n production | grep -v "deployment" | grep -v "REVISION" | awk '{print $1}' > version.csv
+kubectl rollout history deploy/jenkins-demo -n <namespace> | grep -v "deployment" | grep -v "REVISION" | awk '{print $1}' > version.csv
